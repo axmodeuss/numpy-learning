8a8f1a2b8de9c91d92bb5e3cd29f7edcb6ec3b85
axmodeuss <pouyan.h1379@gmail.com>
2026-08-01 17:29:05 +0300

    learn numpy zeros ones and full array creation

--- lessons/lesson2_array_creation.py
+++ lessons/lesson2_array_creation.py
@@ -1,17 +1,14 @@
 import numpy as np
 
-
-
-arr1 = np.array([10,20,30,40,50])
+arr1 = np.zeros(10,dtype=int)
 print(f"{10*'='}{arr1}{10*'='}")
 print(f"dtype : {arr1.dtype} \nshape: {arr1.shape} \nsize: {arr1.size}\nndim : {arr1.ndim}")
-arr2 = np.array([1.5,2.5,3.5])
-print(f"{10*'='}{arr2}{10*'='}")
+
+arr2 = np.ones((4,5))
+print(f"{10*'='}\n{arr2}\n{10*'='}")
 print(f"dtype : {arr2.dtype} \nshape: {arr2.shape} \nsize: {arr2.size}\nndim : {arr2.ndim}")
-arr3 = np.array(["python","numpy","pandas"])
+
+arr3 = np.full((3,3),9)
 print(f"{10*'='}{arr3}{10*'='}")
 print(f"dtype : {arr3.dtype} \nshape: {arr3.shape} \nsize: {arr3.size}\nndim : {arr3.ndim}")
-arr4 = np.random.randint(10,100,20)
-print(f"{10*'='}{arr4}{10*'='}")
-print(f"dtype : {arr4.dtype} \nshape: {arr4.shape} \nsize: {arr4.size}\nndim : {arr4                    .ndim}")
 
