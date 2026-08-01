ddd71bebe76d08d1985b4c4f33aec15f1899d4d0
axmodeuss <pouyan.h1379@gmail.com>
2026-08-01 17:42:11 +0300

    learn numpy array creation methods

--- lessons/lesson2_array_creation.py
+++ lessons/lesson2_array_creation.py
@@ -1,14 +1,42 @@
 import numpy as np
 
+def show_info(arr):
+    print("="*30)
+    print(arr)
+    print(f"""
+dtype : {arr.dtype}
+shape : {arr.shape}
+size  : {arr.size}
+ndim  : {arr.ndim}
+""")
+
 arr1 = np.zeros(10,dtype=int)
 print(f"{10*'='}{arr1}{10*'='}")
-print(f"dtype : {arr1.dtype} \nshape: {arr1.shape} \nsize: {arr1.size}\nndim : {arr1.ndim}")
+show_info(arr1)
 
 arr2 = np.ones((4,5))
 print(f"{10*'='}\n{arr2}\n{10*'='}")
-print(f"dtype : {arr2.dtype} \nshape: {arr2.shape} \nsize: {arr2.size}\nndim : {arr2.ndim}")
+show_info(arr2)
 
 arr3 = np.full((3,3),9)
 print(f"{10*'='}{arr3}{10*'='}")
-print(f"dtype : {arr3.dtype} \nshape: {arr3.shape} \nsize: {arr3.size}\nndim : {arr3.ndim}")
+show_info(arr2)
 
+print(10*"=")
+
+print(np.arange(0,50,5))
+print(np.arange(10,101,10))
+print(np.linspace(0,1,10))
+print(np.linspace(100,200,5))
+
+arr4= np.arange(0,100,2)
+show_info(arr4)
+
+arr5= np.linspace(-5,5,20)
+show_info(arr5)
+
+arr6= np.arange(2000,2026)
+show_info(arr6)
+
+arr7=np.linspace(15,25,7)
+show_info(arr7)
