a182de0c027f7d1cf6fa9a6d967e146e36ffb980
axmodeuss <pouyan.h1379@gmail.com>
2026-08-03 17:29:31 +0300

    broadcasting

---
+++ lessons/lesson10_broadcasting.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+#1
+# a = np.array([2,4,6])
+
+# print(a+100)
+
+#2
+# a = np.array([
+#     [1,2],
+#     [3,4]
+# ])
+
+# b = np.array([10,20])
+
+# print(a+b)
+
+
+#3
+# a = np.array([
+#     [1],
+#     [2],
+#     [3]
+# ])
+
+# b = np.array([100,200,300])
+
+# print(a+b)
+
+
+# 4
+# a = np.array([1,2])
+
+# b = np.array([10,20,30])
+
+# print(a+b)
+
