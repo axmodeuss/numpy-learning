780dee14f2b123477aeefdfc4df8e31074fda338
axmodeuss <pouyan.h1379@gmail.com>
2026-08-04 11:31:51 +0300

    Learn numpy where conditional selection

---
+++ lessons/lesson12_where.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+numbers = np.array([1,5,10,15,20])
+
+numbers_morethan10 = np.where(numbers>10,"Big","Small")
+print(numbers_morethan10)
+
+scores = np.array([20,55,90,40,75])
+passed_scores = np.where(scores>=50,"Passed","Failed")
+print(passed_scores)
+
+ages = np.array([12,18,25,9,40])
+ages_above18= np.where(ages>18,"adult","child")
+print(ages_above18)
+
+import pandas as pd
+
+import pandas as pd
+
+df = pd.DataFrame({
+    "score":[40,70,90,30]
+})
+
+df["Status"] = np.where(df["score"]>=50,"Passed","Failed")
+print(df)
