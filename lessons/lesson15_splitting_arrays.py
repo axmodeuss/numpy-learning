88bdb0705fdf8eb4b0714b7a40125b93856a7699
axmodeuss <pouyan.h1379@gmail.com>
2026-08-04 16:57:50 +0300

    learn numpy array splitting

---
+++ lessons/lesson15_splitting_arrays.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+arr = np.arange(1,25)
+
+arr_splited = np.split(arr,4)
+print(arr_splited)
+
+
+arr_array_split = np.arange(15)
+print(np.array_split(arr_array_split,4))
+
+matrix = np.array([
+    [10,20],
+    [30,40],
+    [50,60],
+    [70,80]
+])
+print(np.hsplit(matrix,2))
+
+
+sales = np.array([
+    [120,180,210],
+    [200,220,250],
+    [150,170,190]
+])
+splited = np.hsplit(sales,3)
+for i in range(3):
+    print(np.mean(splited[i]))
