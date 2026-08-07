8928b030ae346747e4f29431a00a92949902cf68
axmodeuss <pouyan.h1379@gmail.com>
2026-08-07 09:22:54 +0300

    learn numpy linear algebra basics

--- lessons/lesson23_linear_algebra.py
+++ lessons/lesson23_linear_algebra.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-a= np.array([[3,2],[5,4]])
-b= np.array([16,28])
+# a= np.array([[3,2],[5,4]])
+# b= np.array([16,28])
+a= np.array([[3,2],[5,7]])
+b= np.array([18,39])
 
-print(np.linalg.solve(a,b))
+print(np.linalg.solve(a,b).round(2))
